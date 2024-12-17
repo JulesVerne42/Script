06e6227aaac71369a7ac61aabeafbd045945d648
Willy <158064110+JulesVerne42@users.noreply.github.com>
2024-12-17 17:04:32 +0100

    Add files via upload

---
+++ Functions.psm1
@@ -0,0 +1,18 @@
+﻿function Log
+{
+    param([string]$FilePath,[string]$Content)
+
+    # Vérifie si le fichier existe, sinon le crée
+    If (-not (Test-Path -Path $FilePath))
+    {
+        New-Item -ItemType File -Path $FilePath | Out-Null
+    }
+
+    # Construit la ligne de journal
+    $Date = Get-Date -Format "dd/MM/yyyy-HH:mm:ss"
+    $User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    $logLine = "$Date;$User;$Content"
+
+    # Ajoute la ligne de journal au fichier
+    Add-Content -Path $FilePath -Value $logLine
+}
---
+++ Main.ps1
@@ -0,0 +1,2 @@
+﻿# Q.2.2 Changement du chemin du script
+Start-Process -FilePath "powershell.exe" -ArgumentList "C:\Scripts\AddLocalUsers.ps1" -Verb RunAs -WindowStyle Maximized
