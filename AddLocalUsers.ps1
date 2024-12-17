b47ee5f0836139faf63b09a70bdcf60eea60f350
Willy <158064110+JulesVerne42@users.noreply.github.com>
2024-12-17 16:57:24 +0100

    Add files via upload

---
+++ AddLocalUsers.ps1
@@ -0,0 +1,40 @@
+Function Random-Password ($length = 6){    $punc = 46..46
+    $digits = 48..57
+    $letters = 65..90 + 97..122
+    $password = get-random -count $length -input ($punc + $digits + $letters) |`
+        ForEach -begin { $aa = $null } -process {$aa += [char]$_} -end {$aa}
+    Return $password.ToString()
+}
+
+Function ManageAccentsAndCapitalLetters{    param ([String]$String)    
+    $StringWithoutAccent = $String -replace '[éèêë]', 'e' -replace '[àâä]', 'a' -replace '[îï]', 'i' -replace '[ôö]', 'o' -replace '[ùûü]', 'u'    $StringWithoutAccentAndCapitalLetters = $StringWithoutAccent.ToLower()    $StringWithoutAccentAndCapitalLetters
+}
+
+$Path = "C:\Scripts"$CsvFile = "$Path\Users.csv"$LogFile = "$Path\Log.log"
+# Q.2.3
+# Q.2.5
+$Users = Import-Csv -Path $CsvFile -Delimiter ";" -Header "prenom","nom","societe","fonction","service","description","mail","mobile","scriptPath","telephoneNumber" -Encoding UTF8  | Select-Object -Skip 2
+foreach ($User in $Users)
+{
+    $Prenom = ManageAccentsAndCapitalLetters -String $User.prenom    $Nom = ManageAccentsAndCapitalLetters -String $User.Nom
+    $Name = "$Prenom.$Nom"
+    If (-not(Get-LocalUser -Name "$Prenom.$Nom" -ErrorAction SilentlyContinue))    {        $Pass = Random-Password        $Password = (ConvertTo-secureString $Pass -AsPlainText -Force)
+        $Description = "$($user.description) - $($User.fonction)"
+        # Q.2.4
+        # Q.2.11
+        $UserInfo = @{
+            Name                 = "$Prenom.$Nom"
+            FullName             = "$Prenom.$Nom"
+            Password             = $Password
+            AccountNeverExpires  = $true
+            PasswordNeverExpires = $false
+        }
+
+        New-LocalUser @UserInfo
+        #Q.2.10
+        Add-LocalGroupMember -Group "Utilisateur" -Member "$Prenom.$Nom"
+        # Q.2.6
+        Write-Host "L'utilisateur $Prenom.$Nom a été crée"
+    }
+    # Q.2.9
+}
